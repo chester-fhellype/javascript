9ccb10457107a1c79272b9daf01ad6292ec18f6c
chester-fhellype <57117332+chester-fhellype@users.noreply.github.com>
2019-10-28 22:27:50 -0300

    Adicionei 3 pastas da aula 1 de java script

---
+++ funcoes/hello.js
@@ -0,0 +1,11 @@
+/**
+ * Exemplo de uma função simples
+ * @author Chester Fhellype
+ */
+
+
+ function helloWorld(){
+     let nome
+     nome = prompt ('Qual é o seu nome?')
+     alert ('Hello' + nome)
+ }
---
+++ vincularJS/documento.js
@@ -0,0 +1,6 @@
+/**
+ * Exemplo de vinculação de um documento JS no html
+ * @author Chester fhellype
+ */
+
+ document.write('<h2>'+'hello world'+'</h2>')
